ff4ebff86d6ac98671de0b520252b6d4ae3650bb
DANKAHIA <dankahia86@gmail.com>
2023-03-20 09:49:08 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main function
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (1);
+}
+
