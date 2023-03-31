fa37b9953024c8c225d65dd3e248d6c38672da11
DANKAHIA <dankahia86@gmail.com>
2023-03-31 08:47:46 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,105 +1,71 @@
 #include "main.h"
-
 /**
- * len - to get the length of a sting
- * @str: string
- *
- * Return: length
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
  */
-
-int len(char *str)
+void rev_string(char *n)
 {
-	int l = 0, i;
-
-	for (i = 0; str[i] != '\0'; i++)
-		l++;
-	return (l);
-}
-
-/**
- * add_string - it adds two numbers that are string
- * @n1: the first number
- * @n2: second number
- * @r: the string to return
- *
- * Return: r - addition of n1 and n2
- */
-char *add_string(char *n1, char *n2, char *r)
+int i = 0;
+int j = 0;
+char temp;
+while (*(n + i) != '\0')
 {
-	int num_1, num_2, add, l1, l2, rr = 0, j = 0;
-
-	l1 = len(n1) - 1, l2 = len(n2) - 1;
-	while (l1 >= 0 && l2 >= 0)
-	{
-		num_1 = n1[l1] - 48;
-		num_2 = n2[l2] - 48;
-		add = num_1 + num_2 + rr;
-		r[j] = (add % 10) + 48;
-		rr = add / 10;
-		l1--;
-		l2--;
-		j++;
-	}
-	while (l1 >= 0 && l2 < 0)
-	{
-		num_1 = n1[l1] - 48;
-		add = num_1 + rr;
-		r[j] = (add % 10) + 48;
-		rr = add / 10;
-		l1--;
-		j++;
-	}
-	while (l2 >= 0 && l1 < 0)
-	{
-		num_2 = n2[l2] - 48;
-		add = num_2 + rr;
-		r[j] = (add % 10) + 48;
-		rr = add / 10;
-		l2--;
-		j++;
-	}
-	if (l1 < 0 && l2 < 0 && rr > 0)
-	{
-		r[j] = rr + 48;
-	}
-	r[j + 1] = '\0';
-	return (r);
+i++;
 }
-
-
+i--;
+for (j = 0; j < i; j++, i--)
+{
+temp = *(n + j);
+*(n + j) = *(n + i);
+*(n + i) = temp;
+}
+}
 /**
- * infinite_add - a function that adds two numbers.
- * @n1: first number
- * @n2: second number
- * @r: is the buffer that the function will use to store the result
- * @size_r: is the buffer size
- *
- * Return: returns r - pointer to the result
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int j = 0, len_;
-	char temp;
-
-	size_r--;
-	if (len(n1) > size_r || len(n2) > size_r)
-		return (0);
-
-	r = add_string(n1, n2, r);
-
-	if (len(r) > size_r)
-		return (0);
-
-	len_ = len(r) - 1;
-	for (j = 0; j < len_; j++)
-	{
-		temp = r[j];
-
-		r[j] = r[len_];
-		r[len_] = temp;
-
-		len_--;
-	}
-	return (r);
-}}
+int overflow = 0, i = 0, j = 0, digits = 0;
+int val1 = 0, val2 = 0, temp_tot = 0;
+while (*(n1 + i) != '\0')
+i++;
+while (*(n2 + j) != '\0')
+j++;
+i--;
+j--;
+if (j >= size_r || i >= size_r)
+return (0);
+while (j >= 0 || i >= 0 || overflow == 1)
+{
+if (i < 0)
+val1 = 0;
+else
+val1 = *(n1 + i) -'0';
+if (j < 0)
+val2 = 0;
+else
+val2 = *(n2 + j) -'0';
+temp_tot = val1 + val2 + overflow;
+if (temp_tot >= 10)
+overflow = 1;
+else
+overflow = 0;
+if (digits >= (size_r - 1))
+return (0);
+*(r + digits) = (temp_tot % 10) + '0';
+digits++;
+j--;
+i--;
+}
+if (digits == size_r)
+return (0);
+*(r + digits) = '\0';
+rev_string(r);
+return (r);
+}
