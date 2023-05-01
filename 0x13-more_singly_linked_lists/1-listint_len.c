7dbc5f8d8ada6ccc3f40dc7ed055c21e171edaf1
DANKAHIA <dankahia86@gmail.com>
2023-05-01 12:55:39 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,12 +7,12 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size_t nnodes = 0;
+	size_t i = 0;
 
 	while (h != NULL)
 	{
+		i++;
 		h = h->next;
-		nnodes++;
 	}
-	return (nnodes);
+	return (i);
 }
