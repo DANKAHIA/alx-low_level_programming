2722a0bda2c0d6594d32e9aded5e6c0ff964d3d4
DANKAHIA <dankahia86@gmail.com>
2023-08-25 04:07:28 -0700

    functions.c

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,69 @@
+#include "main.h"
+
+/*
+ * _memcpy - copy string.
+ * @dest: destination
+ * @src: source
+ * @n: number of byets.
+ * 
+ * Return: string
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    return (0);
+}
+
+/*
+ * _strchr - function that does something
+ * I do not know but it's important
+ * to the task
+ * 
+ * @s: string
+ * @c: string
+ * 
+ * Return: string
+ */
+
+char *_strchr(char *s, char c)
+{
+    return (0);
+}
+
+/*
+ * _strspn - function
+ * @s: string
+ * @accept: string
+ * 
+ * Return: unsigned int
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+    return (0);
+}
+
+/*
+ * _strpbrk - function
+ * @s: string
+ * @accept: string
+ * 
+ * Return: string
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+    return (0);
+}
+/*
+ * _strstr - function
+ * @haystack: string
+ * @needle: string
+ * 
+ * Return: string 
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+    return (0);
+}
